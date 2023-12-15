aaeddb3978d6169e570414364876ad014897734e
Ryan <rn.billaud@gmail.com>
2023-12-15 16:30:16 +0100

    Label Fait

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var middle = canvas.width / 2;
 ctx.textAlign = "right";
 var ch = "";
 var mode = 0;
+var zoom = 0;
 
 function on_off() {
     if (canvas.style.backgroundColor == "black") {
@@ -90,4 +91,29 @@ function affiche_quadrillage() {
         ctx.lineTo(canvas.width - 20, y);
     }
     ctx.stroke();
+}
+
+function zoom_in(){
+    if (zoom < 2){
+        zoom += 1;
+        console.log(zoom);
+    }
+    
+}
+
+function zoom_out(){
+    if (zoom > 0){
+        zoom -= 1;
+    }
+    console.log(zoom);
+}
+
+function labels(){
+    if (zoom == 0){
+        return ["-45","-40","-35","-30","-25","-20","-15","-10","-5","0","5","10","15","20","25","30","35","40","45","50"];
+    } else if (zoom == 1){
+        return ["-27","-24","-21","-18","-15","-12","-9","-6","-3","0","3","6","9","12","15","18","21","24","27","30"];
+    } else {
+        return ["-9","-8","-7","-6","-5","-4","-3","-2","-1","0","1","2","3","4","5","6","7","8","9","10"];
+    }
 }
